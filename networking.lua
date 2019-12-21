e28ba412126f14f6c2c80a3246e049cf1c897344
mitterdoo <connorashcroft@live.com>
2019-12-21 02:01:19 -0800

    Changed outline for networking

--- networking.lua
+++ networking.lua
@@ -7,15 +7,13 @@
 	Additionally, there are Games. Alice's CLIENT has one Game: their own. They communicate to the SERVER, which is running an identical Game specifically for Alice.
 	Because a Game for a CLIENT must be identical to the SERVER's dedicated Game for that player, Games must also keep track of information, including:
 		Player's playfield (BRIX game)
-		Alive players
-		Other players' badges
-		Other players' fields
-		Other players targeting this player
 		
 	
 	CLIENT Deltas may consist of:
 		Every input event
-		Targeting tactics (manual or attackers/k.o.'s/badges/random; IDs are not sent)
+		Targeting tactics, can be either:
+			Single ID of player to target, or
+			No ID, implying targeting Attackers
 		Game Over (to a player or self)
 		Delta acknowledge
 
@@ -39,15 +37,23 @@
 	As the CLIENT plays, their inputs are recorded to their current Delta Queue.
 	If they receive a SERVER Delta, it must be immediately acknowledged, by getting added to the Delta Queue.
 		Events received in the Delta are displayed immediately to the CLIENT (k.o.'s, sent garbage, targeting, etc.)
-	If their game ends for any reason, the CLIENT must send the frame at which its game ended.
-	After a specified amount of time, the CLIENT Delta Queue is sent to the SERVER.
+	If their game ends for any reason, the CLIENT must send the frame, at which its game ended, in the next Delta.
+	At a set interval, the current CLIENT Delta Queue is sent to the SERVER.
 
 	When the SERVER receives a Delta from a CLIENT, each event is iterated through, in order:
-		If the event is an input, or targeting tactics, it is "plugged in" to the CLIENT's linked Game (hosted on the SERVER)
-			If the player sends garbage, or changes targets, those events are queued in the SERVER Delta Queue.
-			Furthermore, if the receiving player of garbage/target is currently dead, disregard it.
+		If the event is an input, it is "plugged in" to the CLIENT's linked Game (hosted on the SERVER)
+			If the player sends garbage, those events are queued in the SERVER Delta Queue.
+			Furthermore, if the receiving player of garbage is currently dead, disregard it.
+		If the event is a change of targets, queue the target switch to the SERVER Delta Queue
+			If the individual target is currently dead, pick a random player until the CLIENT gets the next SERVER Delta.
+			If there are no attackers for this player, pick a random player as well.
 		If the event is a Game Over, K.O. is added to SERVER Delta Queue, and SERVER no longer listens to this CLIENT.
 		If the event is a Delta Acknowledge, the player's SERVERside simulated game is adjusted accordingly, plugging in the specified Delta's events, just like the CLIENT
+			Only Garbage/Badges/K.O.'s regarding this player are considered on SERVER.
 
+
+	Which players to target shall only be done on the CLIENT. The CLIENT should handle picking a near-death player, high-badge player, and random player.
+		When switching to Attackers, if the player does not have any, pick a random player and stay on them, until attacked or mode change
+	
 
 ]]
