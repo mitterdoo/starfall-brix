7a55d9ec7eb166ac6e831102ee693a4fafb84e73
mitterdoo <connorashcroft@live.com>
2019-12-21 23:26:17 -0800

    Refined networking outline for planning

--- networking.lua
+++ networking.lua
@@ -1,25 +1,25 @@
 --[[
 	The idea:
-	CLIENT and SERVER both have Deltas, and Delta Queues. Information is queued up, and then sent as Deltas, be it CLIENT to SERVER, or vice versa.
-	A Delta generally contains the "delta" from the last game state, and the current game state.
-	The Delta send rate for both realms, must be the same. For example, 5 Deltas a second.
+	CLIENT and SERVER both have Snapshots, and Snapshot Queues. Information is queued up, and then sent as Snapshots, be it CLIENT to SERVER, or vice versa.
+	A Snapshot generally contains the "Snapshot" from the last game state, and the current game state.
+	The Snapshot send rate for both realms, must be the same. For example, 5 Snapshots a second.
 
 	Additionally, there are Games. Alice's CLIENT has one Game: their own. They communicate to the SERVER, which is running an identical Game specifically for Alice.
 	Because a Game for a CLIENT must be identical to the SERVER's dedicated Game for that player, Games must also keep track of information, including:
 		Player's playfield (BRIX game)
 		
 	
-	CLIENT Deltas may consist of:
+	CLIENT Snapshots may consist of:
 		Every input event
 		Targeting tactics, can be either:
 			Single ID of player to target, or
 			No ID, implying targeting Attackers
 		Game Over (to a player or self)
-		Delta acknowledge
+		Snapshot acknowledge
 
 		These must be sent in chronological order, and are all sent containing the time at which they occurred.
 	
-	SERVER Deltas may consist of:
+	SERVER Snapshots may consist of:
 			<combat>
 		Alice sends Bob X garbage lines
 		Alice targets {list} players
@@ -32,24 +32,26 @@
 		Alice clears X lines
 	
 
-		SERVER Deltas are not ordered. They must contain a unique Delta identifier.
+		SERVER Snapshots are not ordered. They must contain a unique Snapshot identifier.
 	
-	As the CLIENT plays, their inputs are recorded to their current Delta Queue.
-	If they receive a SERVER Delta, it must be immediately acknowledged, by getting added to the Delta Queue.
-		Events received in the Delta are displayed immediately to the CLIENT (k.o.'s, sent garbage, targeting, etc.)
-	If their game ends for any reason, the CLIENT must send the frame, at which its game ended, in the next Delta.
-	At a set interval, the current CLIENT Delta Queue is sent to the SERVER.
+	As the CLIENT plays, their inputs are recorded to their current Snapshot Queue.
+	If they receive a SERVER Snapshot, it must be immediately acknowledged, by getting added to the Snapshot Queue.
+		Events received in the Snapshot are displayed immediately to the CLIENT (k.o.'s, sent garbage, targeting, etc.)
+	If their game ends for any reason, the CLIENT must send the frame, at which its game ended, in the next Snapshot.
+	At a set interval, the current CLIENT Snapshot Queue is sent to the SERVER.
 
-	When the SERVER receives a Delta from a CLIENT, each event is iterated through, in order:
+	When the SERVER receives a Snapshot from a CLIENT, each event is iterated through, in order:
 		If the event is an input, it is "plugged in" to the CLIENT's linked Game (hosted on the SERVER)
-			If the player sends garbage, those events are queued in the SERVER Delta Queue.
+			If the player sends garbage, those events are queued in the SERVER Snapshot Queue.
 			Furthermore, if the receiving player of garbage is currently dead, disregard it.
-		If the event is a change of targets, queue the target switch to the SERVER Delta Queue
-			If the individual target is currently dead, pick a random player until the CLIENT gets the next SERVER Delta.
+		If the event is a change of targets, queue the target switch to the SERVER Snapshot Queue
+			If the individual target is currently dead, pick a random player until the CLIENT gets the next SERVER Snapshot.
 			If there are no attackers for this player, pick a random player as well.
-		If the event is a Game Over, K.O. is added to SERVER Delta Queue, and SERVER no longer listens to this CLIENT.
-		If the event is a Delta Acknowledge, the player's SERVERside simulated game is adjusted accordingly, plugging in the specified Delta's events, just like the CLIENT
-			Only Garbage/Badges/K.O.'s regarding this player are considered on SERVER.
+		If the event is a Game Over, K.O. is added to SERVER Snapshot Queue, and SERVER no longer listens to this CLIENT.
+		If the event is a Snapshot Acknowledge, the player's SERVERside simulated game is adjusted accordingly, plugging in the specified Snapshot's events, just like the CLIENT
+			
+			Only Garbage/Badges/K.O.'s/Targets regarding this player are considered on SERVER.
+				This means, that the SERVER copy of this game, will see what the CLIENT saw at the time.
 
 
 	Which players to target shall only be done on the CLIENT. The CLIENT should handle picking a near-death player, high-badge player, and random player.
@@ -57,3 +59,5 @@
 	
 
 ]]
+
+
