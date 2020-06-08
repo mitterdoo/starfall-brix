b667f7c70987e35130cc6be8f22770126165a0bc
mitterdoo <connorashcroft@live.com>
2020-06-08 00:52:02 -0700

    Added Sprite control

---
+++ client/gui/sprite.lua
@@ -0,0 +1,54 @@
+local PANEL = {}
+
+local setSheet = sprite.setSheet
+local drawSprite = sprite.draw
+
+function PANEL:Init()
+	self.sheet = 1
+	self.sprite = 0
+	self.trueSize = true
+	self.color = Color(255, 255, 255)
+end
+
+function PANEL:SetColor(col)
+	self.color = col
+end
+
+function PANEL:SetSheet(sheet)
+	assert(type(sheet) == "number", "sheet must be number")
+	self.sheet = sheet
+end
+
+function PANEL:SetSprite(sprite)
+	assert(type(sprite) == "number", "sprite must be number")
+	self.sprite = sprite
+end
+
+function PANEL:OnSizeChanged(w, h)
+	self.trueSize = false
+end
+
+function PANEL:SetTrueSize(isTrue)
+	self.trueSize = isTrue
+end
+
+function PANEL:RealSize()
+
+	local _, _, w, h = sprite.sheets[self.sheet][self.sprite]
+	self:SetSize(w, h)
+
+end
+
+function PANEL:Paint(w, h)
+
+	setSheet(self.sheet)
+	render.setColor(self.color)
+	if self.trueSize then
+		drawSprite(self.sprite, 0, 0)
+	else
+		drawSprite(self.sprite, 0, 0, w, h)
+	end
+
+end
+
+gui.Register("Sprite", PANEL, "Control")
