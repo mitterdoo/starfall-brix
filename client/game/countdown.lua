5273cbb9735e286ab3553e2426b4b56b6d24c4a8
mitterdoo <connorashcroft@live.com>
2020-07-06 22:01:43 -0700

    "Matching" and "Get ready" message before beginning

--- client/game/countdown.lua
+++ client/game/countdown.lua
@@ -2,14 +2,22 @@
 hook.add("brConnect", "countdown", function(game, arena)
 
 	local textSprite, numberSprite
+	local root = game.controls.root
+	local center_x, center_y = root.w/2, root.h/2
+	local getReady = gui.Create("Sprite", root)
+	getReady:SetSheet(1)
+	getReady:SetSprite(65)
+	getReady:SetAlign(0, 0)
+	getReady:SetPos(center_x, center_y / 2 * 3)
 
 	arena.hook("init", function()
 	
-		local root = game.controls.root
-		local center_x, center_y = root.w/2, root.h/4
+		if getReady then
+			getReady:Remove()
+		end
 
 		textSprite = gui.Create("Sprite", root)
-		textSprite:SetPos(center_x, center_y)
+		textSprite:SetPos(center_x, center_y/2)
 		textSprite:SetAlign(0, 0)
 		textSprite:SetSheet(1)
 		textSprite:SetSprite(67)
@@ -19,7 +27,7 @@ hook.add("brConnect", "countdown", function(game, arena)
 		numberSprite:SetColor(Color(255, 255, 0))
 		numberSprite:SetSize(96, 80)
 		numberSprite:SetAlign(0)
-		numberSprite:SetPos(center_x, center_y - 40)
+		numberSprite:SetPos(center_x, center_y/2 - 40)
 		numberSprite:SetVisible(false)
 
 		local counter = 0
@@ -42,6 +50,12 @@ hook.add("brConnect", "countdown", function(game, arena)
 				textSprite:Remove()
 			end
 		end)
+
+	end)
+
+	arena.hook("arenaFinalized", function()
+
+		getReady:SetSprite(66)
 
 	end)
 
