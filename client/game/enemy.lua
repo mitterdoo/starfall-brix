9f07221a77e0f08ed3980107c1f7c4301f90eca1
mitterdoo <connorashcroft@live.com>
2020-07-23 01:12:17 -0700

    Display garbage between other players

--- client/game/enemy.lua
+++ client/game/enemy.lua
@@ -304,5 +304,123 @@ hook.add("brConnect", "enemy", function(game, arena)
 
 	end)
 
+	local framePos, frameSize = Vector(0, 0, 0), Vector(0, 0, 0)
+	do
+
+		local scale
+		framePos, scale = LayerAbove:AbsolutePos(Vector(148, 52, 0))
+		frameSize = Vector(728, 920, 0) * scale
+
+	end
+
+	local group_backgroundAttacks = {
+		enter = function()
+
+			render.setStencilWriteMask(0xFF)
+			render.setStencilTestMask(0xFF)
+			render.setStencilReferenceValue(0)
+			render.setStencilCompareFunction(STENCIL_ALWAYS)
+			render.setStencilPassOperation(STENCIL_KEEP)
+			render.setStencilFailOperation(STENCIL_KEEP)
+			render.setStencilZFailOperation(STENCIL_KEEP)
+			render.clearStencil()
+			render.setStencilEnable(true)
+
+			render.setStencilReferenceValue(1)
+			render.setStencilPassOperation(STENCIL_REPLACE)
+			render.setRGBA(255, 255, 255, 0)
+			render.drawRect(framePos[1], framePos[2], frameSize[1], frameSize[2])
+
+			render.setStencilCompareFunction(STENCIL_NOTEQUAL)
+
+		end,
+		exit = function()
+
+			render.setStencilEnable(false)
+
+		end
+	}
+
+	local function garbageTravel(attackerID, damage, targetID)
+
+		local attackerCtrl = enemies[attackerID]
+		local targetCtrl = enemies[targetID]
+
+		if attackerCtrl == nil then return end
+		local enemySize = Vector(attackerCtrl.w, attackerCtrl.h, 0)
+		local attackerPos, scale = attackerCtrl:AbsolutePos(enemySize/2)
+		local overlay = false
+		if not targetCtrl then
+			if targetID ~= arena.uniqueID then
+				error("Tried to make garbage anim for unknown target ID " .. tostring(targetID))
+			end
+			targetCtrl = LayerBelow
+			overlay = true
+		end
+		local targetPos = targetCtrl:AbsolutePos(Vector(targetCtrl.w/2, targetCtrl.h/2, 0))
+
+		local percent = damage / arena.params.maxGarbageOut
+		local size = Vector(1, 1, 0) * (32 + 32*percent) * scale
+
+		gfx.EmitParticle(
+			{attackerPos, targetPos},
+			{size, size},
+			0, 0.5,
+			fx_AttackTravel,
+			true, true,
+			nil, not overlay and group_backgroundAttacks
+		)
+
+		gfx.EmitParticle(
+			{targetPos, targetPos},
+			{enemySize*scale, enemySize*scale*1.2},
+			0.5, 0.1,
+			fx_AttackLand,
+			true, true
+		)
+
+	end
+
+	arena.hook("playerGarbage", function(attackerID, damage, targets)
+	
+		for _, targetID in pairs(targets) do
+
+			garbageTravel(attackerID, damage, targetID)
+
+		end
+
+	end)
+
+	hook.add("DEBUG", "ag", function()
+	
+		local players = {}
+		local playerCount = 0
+		for id, enemy in pairs(arena.arena) do
+			if not enemy.dead then
+				table.insert(players, id)
+				playerCount = playerCount + 1
+			end
+		end
+
+
+		local shuffledPlayers = {}
+		for i = playerCount, 1, -1 do
+			local index = math.random(i)
+			local enemy = table.remove(players, index)
+			table.insert(shuffledPlayers, enemy)
+		end
+
+		local attackerID = table.remove(shuffledPlayers, 1)
+		if attackerID == arena.uniqueID then return end
+		for i = 1, 3 do
+			local targetID = table.remove(shuffledPlayers, 1)
+			if targetID then
+				garbageTravel(attackerID, 1, targetID)
+			end
+		end
+
+		garbageTravel(attackerID, 1, arena.uniqueID)
+
+	end)
 end)
 
