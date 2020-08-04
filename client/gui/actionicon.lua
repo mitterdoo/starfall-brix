06a7093258e975de9a971842708b0d6aa78cf9a0
mitterdoo <connorashcroft@live.com>
2020-08-03 18:56:39 -0700

    ActionIcon Control

---
+++ client/gui/actionicon.lua
@@ -0,0 +1,21 @@
+local PANEL = {}
+
+function PANEL:Init()
+	self.super.Init(self)
+	self.action = "ui_accept"
+end
+
+function PANEL:SetAction(action)
+	self.action = action
+end
+
+function PANEL:Think()
+	local binding = binput.getBinding(self.action, binput.isController)
+
+	if binding then
+		self.input = binding
+	end
+
+end
+
+gui.Register("ActionIcon", PANEL, "InputIcon")
